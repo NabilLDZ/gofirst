4e6a9adb94ff27ed1a496a0312f2bc3058bc4ea4
unknown <nabilcolabsprojek@gmail.com>
2025-10-19 08:17:44 +0700

    test pt 2

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Dunia test CICD pt")
+	fmt.Fprintf(w, "Hello Dunia test CICD pt test lagi kaka")
 }
 
 func main() {
